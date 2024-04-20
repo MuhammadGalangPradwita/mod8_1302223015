663408bf84133a3fa427e7003097fc8527d6611a
braacellettt <104008593+GalangPradwita@users.noreply.github.com>
2024-04-20 08:05:34 +0700

    first commit

---
+++ modul8_1302223015/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace modul8_1302223015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
